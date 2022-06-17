54ab771d1c344f60349fac9bd7288b5699fb94b8
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-17 14:09:12 +0530

    update builder
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,16 +1,19 @@
 #/bin/bash
+set -e
+set -o pipefail
 
 REPO=utkarsh-pro/builder
 NEW_SCRIPT=${1:-script.sh}
+WD=$(pwd)
 
 # Clone the builder repo
-git clone $REPO $TMPDIR/builder
+git clone https://github.com/$REPO.git ${TMPDIR}builder
 
 # Move the new script to the builder repo
-mv $NEW_SCRIPT $TMPDIR/builder/script.sh
+mv $NEW_SCRIPT ${TMPDIR}builder/script.sh
 
 # Add and commit the new script
-cd $TMPDIR/builder
+cd ${TMPDIR}builder
 git add script.sh
 git commit -m "Update script"
 
@@ -18,17 +21,13 @@ git commit -m "Update script"
 git push origin master
 
 # Get back to the previous working directory
-cd -
+cd $WD
 
 # Remove the builder repo
-rm -rf $TMPDIR/builder
+rm -rf ${TMPDIR}builder
 
 # Download the artifacts produced
 RUN_ID=`gh run list -L 1 -w build --json databaseId -t '{{ (index . 0).databaseId | printf "%.f" }}' -R $REPO`
 
 gh run view -i 1 -R $REPO $RUN_ID --log --exit-status
-if [ $? -ne 0 ]; then
-	exit 1
-fi
-
 gh run download $RUN_ID -R $REPO
