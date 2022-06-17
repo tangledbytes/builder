114ef144df549c22ae4c3f2fea1992109c786144
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-17 15:57:36 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,19 +1,3 @@
 #/bin/bash
 
-export GOROOT=`go env GOROOT`
-export GOPATH=`go env GOPATH`
-
-echo "Cloning NooBaa Operator..."
-git clone --depth=1 https://github.com/noobaa/noobaa-operator.git
-
-echo "Build NooBaa Operator..."
-cd noobaa-operator
-make gen && make gen-api && make
-
-echo "Dump docker image as tar file..."
-docker save noobaa/noobaa-operator:5.12.0 -o noobaa-operator.tar
-docker save noobaa/noobaa-operator-catalog:5.12.0 -o noobaa-operator-catalog.tar
-
-echo "Uploading tar file to GitHub..."
-mv noobaa-operator.tar $GITHUB_WORKSPACE/artifacts/noobaa-operator.tar
-mv noobaa-operator-catalog.tar $GITHUB_WORKSPACE/artifacts/noobaa-operator-catalog.tar
+echo I was created in the Github Workflow > $GITHUB_WORKSPACE/artifacts/message
