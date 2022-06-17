c9cfc2094808c9527dd3e79b9a48b3c526daaac0
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-17 16:19:13 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-echo "This is a message that was generated in the Github Action" > $GITHUB_WORKSPACE/artifacts/message
+echo "I will just sleep for a while..."
+sleep 60
+echo "This is a message that was generated in the Github Action after sleeping for a while" > $GITHUB_WORKSPACE/artifacts/message
