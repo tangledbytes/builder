8760c836d30b8904763f479f37c835b6ac0680f8
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-17 16:37:37 +0530

    update builder
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- builder.sh
+++ builder.sh
@@ -44,4 +44,5 @@ sleep 15
 RUN_ID=`gh run list -L 1 -w build --json databaseId -t '{{ (index . 0).databaseId | printf "%.f" }}' -R $REPO`
 gh run watch -R $REPO $RUN_ID --exit-status
 gh run view -R $REPO $RUN_ID --log --exit-status | grep "build\tExecute script"
+rm -rf artifact
 gh run download $RUN_ID -R $REPO
