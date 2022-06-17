c313f50820745e211662317de1b60423e3699434
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-17 20:07:01 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,5 +1,20 @@
-#/bin/bash
+#!/bin/bash
 
-echo "I will just sleep for a while..."
-sleep 60
-echo "This is a message that was generated in the Github Action after sleeping for a while" > $GITHUB_WORKSPACE/artifacts/message
+set -e
+
+# Clone the noobaa repository
+git clone --depth=1 -b utkarsh-pro/feature/loadbalancer-cidr-control https://github.com/utkarsh-pro/noobaa-operator.git
+
+cd noobaa-operator
+
+# Build the assets
+make gen && make gen-api && make
+
+# Convert docker images to a tar file
+docker save noobaa/noobaa-operator:5.12.0 > noobaa-operator.tar
+docker save noobaa/noobaa-operator-catalog:5.12.0 > noobaa-operator-catalog.tar
+
+# Upload the assets
+mv noobaa-operator.tar $GITHUB_WORKSPACE/assets/noobaa-operator.tar
+mv noobaa-operator-catalog.tar $GITHUB_WORKSPACE/assets/noobaa-operator-catalog.tar
+mv output $GITHUB_WORKSPACE/assets/output
