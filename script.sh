93493d4e6cbbf9059882b46574539094b5f4d5c2
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-06-29 17:43:04 +0530

    Update script

--- script.sh
+++ script.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-export GOROOT=`go env GOROOT`
-export GOPATH=`go env GOPATH`
-
 function registry() {
 	local IMAGE_NAME=$(uuidgen)
-	local REF=ttl.sh/$IMAGE_NAME:2h
+	local REF=ttl.sh/$IMAGE_NAME:48h
 
 	echo $REF
 }
@@ -21,16 +18,15 @@ function docker_push() {
 }
 
 # Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/feature/multi-ns-bc https://github.com/utkarsh-pro/noobaa-operator.git
+git clone --depth=1 -b utkarsh-pro/cleanup/allowed-buckets https://github.com/utkarsh-pro/noobaa-core.git
 
-cd noobaa-operator
+cd noobaa-core
 
 # Build the assets
-make gen && make gen-api && make
+make noobaa
 
 # Push the docker image to ttl.sh
-docker_push noobaa/noobaa-operator:5.12.0
-docker_push noobaa/noobaa-operator-catalog:5.12.0
+docker_push noobaa
 
 # Upload the assets
 # mv build $GITHUB_WORKSPACE/artifacts/build
