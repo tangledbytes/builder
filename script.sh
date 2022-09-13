e17d2e6c6faf6dced6925694dc1a43f5c51f65b9
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-13 18:03:20 +0530

    Update script

--- script.sh
+++ script.sh
@@ -18,7 +18,7 @@ function docker_push() {
 }
 
 # Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/temp/cleanup/allowed-buckets https://github.com/utkarsh-pro/noobaa-core.git
+git clone --depth=1 -b utkarsh-pro/fix/allowed_buckets https://github.com/utkarsh-pro/noobaa-core.git
 
 cd noobaa-core
 
