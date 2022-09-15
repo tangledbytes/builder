4e667703ab7bea46c8b16eca657c0042f0acb578
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-15 17:28:19 +0530

    Update script

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ git clone --depth=1 -b master https://github.com/noobaa/noobaa-core.git
 # Run tests
 cd noobaa-core
 make tester || exit 1
-docker run --user root --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
+docker run --privileged --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
