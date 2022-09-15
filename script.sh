c908ce515b877e3e38b20168062ef264aa832ba7
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-15 12:01:08 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
-set -e
-
-export GOROOT=`go env GOROOT`
-export GOPATH=`go env GOPATH`
-
-function registry() {
-	local IMAGE_NAME=$(uuidgen)
-	local REF=ttl.sh/$IMAGE_NAME:2d
-
-	echo $REF
-}
-
-function docker_push() {
-	local REF=`registry`
-	docker tag $1 $REF
-	docker push $REF
-
-	echo "[PUSHED]: $1 as $REF"
-}
-
 # Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/temp/feature/multi-ns-bc-2 https://github.com/utkarsh-pro/noobaa-operator.git
+git clone --depth=1 -b utkarsh-pro/temp/upgrade/node-14-to-16 https://github.com/utkarsh-pro/noobaa-core.git
 
-cd noobaa-operator
-
-# Build the assets
-make gen && make gen-api && make
-
-# Push the docker image to ttl.sh
-docker_push noobaa/noobaa-operator:5.12.0
-docker_push noobaa/noobaa-operator-catalog:5.12.0
-
-# Upload the assets
-# mv build $GITHUB_WORKSPACE/artifacts/build
+# Run tests
+make test
