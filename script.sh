85c2a531d8ef0dfa1c26071be5dcfa064c5db576
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-15 17:33:09 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone the noobaa repository
-git clone --depth=1 -b master https://github.com/noobaa/noobaa-core.git
+git clone --depth=1 -b utkarsh-pro/temp/upgrade/node-14-to-16 https://github.com/utkarsh-pro/noobaa-core.git
 
 # Run tests
 cd noobaa-core
