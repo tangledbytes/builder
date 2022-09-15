ce4b71de4ef51af7e06ee171dcbe7d5eecc98cf2
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-15 16:22:03 +0530

    Update script

--- script.sh
+++ script.sh
@@ -7,4 +7,3 @@ git clone --depth=1 -b utkarsh-pro/temp/upgrade/node-14-to-16 https://github.com
 cd noobaa-core
 make tester || exit 1
 docker run --user root --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
-make test-postgres || exit 1
