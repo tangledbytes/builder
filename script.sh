1ff0c281f6274079072382d3611583b671303deb
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-15 17:46:08 +0530

    Update script

--- script.sh
+++ script.sh
@@ -5,5 +5,6 @@ git clone --depth=1 -b utkarsh-pro/temp/upgrade/node-14-to-16 https://github.com
 
 # Run tests
 cd noobaa-core
-make tester || exit 1
-docker run --privileged --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
+npm i
+npm run build
+npm t
