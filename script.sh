6ab6fb433b4978f8f2c0e0a6bda36ad8276940c4
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-19 15:53:31 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-# Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/upgrade/node-14-to-16 https://github.com/utkarsh-pro/noobaa-core.git
+set -e
 
-# Run tests
+function registry() {
+	local IMAGE_NAME=$(uuidgen)
+	local REF=ttl.sh/$IMAGE_NAME:2d
+
+	echo $REF
+}
+
+function docker_push() {
+	local REF=`registry`
+	docker tag $1 $REF
+	docker push $REF
+
+	echo "[PUSHED] $1 => $REF"
+}
+
+# Clone the noobaa repository
+git clone --depth=1 -b utkarsh-pro/fix/anon-user-actions https://github.com/utkarsh-pro/noobaa-core.git
+
 cd noobaa-core
-make test-postgres
+
+# Build the assets
+make noobaa
+
+# Push the docker image to ttl.sh
+docker_push noobaa
+
+# Upload the assets
+# mv build $GITHUB_WORKSPACE/artifacts/build
+# FORCE4
