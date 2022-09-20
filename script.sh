cef83edb1ffcb3000e1bea4fe9d480f85c3c7357
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-20 22:52:12 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-set -e
-
-function registry() {
-	local IMAGE_NAME=$(uuidgen)
-	local REF=ttl.sh/$IMAGE_NAME:2d
-
-	echo $REF
-}
-
-function docker_push() {
-	local REF=`registry`
-	docker tag $1 $REF
-	docker push $REF
-
-	echo "[PUSHED] $1 => $REF"
-}
-
 # Clone the noobaa repository
 git clone --depth=1 -b utkarsh-pro/fix/anon-user-actions https://github.com/utkarsh-pro/noobaa-core.git
 
+# Run tests
 cd noobaa-core
-
-# Build the assets
-make noobaa
-
-# Push the docker image to ttl.sh
-docker_push noobaa
-
-# Upload the assets
-# mv build $GITHUB_WORKSPACE/artifacts/build
-# FORCE4
+make test || exit 1
+make test-postgres || exit 1
+docker run --privileged --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
