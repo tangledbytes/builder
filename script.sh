f172e07ce00bf43b3cb18221dee53d47692b6784
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-21 19:50:03 +0530

    Update script

--- script.sh
+++ script.sh
@@ -5,4 +5,18 @@ git clone --depth=1 -b utkarsh-pro/temp/fix/anon-user-actions https://github.com
 
 # Run tests
 cd noobaa-core
+
+# Git Commit
+COMMIT=$(git log --format="%H" -n 1)
+
+# Check if image already exists on docker hub
+if docker pull ttl.sh/noobaa-tester:$COMMIT; then
+	echo "Image already exists"
+	docker tag ttl.sh/noobaa-tester:$COMMIT noobaa-tester
+else
+	make tester
+	docker tag noobaa-tester ttl.sh/noobaa-tester:$COMMIT
+	docker push ttl.sh/noobaa-tester:$COMMIT
+fi
+
 make test
