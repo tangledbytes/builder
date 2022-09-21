bdaca2429c2fa99cf28c3592179f95e26b18aaf4
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-21 18:51:06 +0530

    Update script

--- script.sh
+++ script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/fix/anon-user-actions https://github.com/utkarsh-pro/noobaa-core.git
+git clone --depth=1 -b utkarsh-pro/temp/fix/anon-user-actions https://github.com/utkarsh-pro/noobaa-core.git
 
 # Run tests
 cd noobaa-core
-make test || exit 1
-make test-postgres || exit 1
-docker run --privileged --rm --name test1 noobaa-tester ./src/test/unit_tests/run_npm_test_on_test_container.sh -s sudo_index.js || exit 1
+make test
