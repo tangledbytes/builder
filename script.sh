d94e05120d1a64b8a2eb92d16eb2c6b492215fa7
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-05-10 14:36:26 +0530

    Update script

--- script.sh
+++ script.sh
@@ -2,34 +2,29 @@
 
 set -e
 
-export GOROOT=`go env GOROOT`
-export GOPATH=`go env GOPATH`
+# Setup a kind cluster
+# kind create cluster
 
-function registry() {
-	local IMAGE_NAME=$(uuidgen)
-	local REF=ttl.sh/$IMAGE_NAME:2d
-
-	echo $REF
-}
-
-function docker_push() {
-	local REF=`registry`
-	docker tag $1 $REF
-	docker push $REF
-
-	echo "[PUSHED]: $1 as $REF"
-}
+# Enable qemu virtualization in docker
+docker run --privileged --rm tonistiigi/binfmt --install all
 
 # Clone the noobaa repository
-git clone --depth=1 -b utkarsh-pro/fix/db-scc-on-upgrade https://github.com/utkarsh-pro/noobaa-operator.git
+git clone --depth=1 -b utkarsh-pro/add/rpm-builds https://github.com/utkarsh-pro/noobaa-core.git
+cd noobaa-core
 
-cd noobaa-operator
-
-# Build the assets
-make gen && make image
-
-# Push the docker image to ttl.sh
-docker_push noobaa/noobaa-operator:5.13.0
+# Build RPM for amd64 and ppc64le
+make rpm PLATFORM=amd64 BUILD_S3SELECT=BUILD_S3SELECT=0
+make rpm PLATFORM=ppc64le BUILD_S3SELECT=BUILD_S3SELECT=0
 
 # Upload the assets
-# mv build $GITHUB_WORKSPACE/artifacts/build
+mv build $GITHUB_WORKSPACE/artifacts/build
+
+# Build the tester
+# make tester
+
+# Upload the tester image to the kind cluster
+# kind load docker-image noobaa-tester
+# kind load docker-image noobaa
+
+# Run the ceph test
+# cd ./src/test/framework/ && ./run_test_job.sh --name s3-tests --image noobaa --tester_image noobaa-tester --tests_list ./system_tests_list.js --job_yaml ../../../.travis/travis_test_job.yaml --wait
